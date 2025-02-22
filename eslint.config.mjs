3915e981c3c3aeadc9559377f80ced98ae506faa
Melonps <kake.kake0311@gmail.com>
2025-02-22 21:50:25 +0900

    [FIX] eslintを緩くする

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,44 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    plugins: ["simple-import-sort"],
+    parser: "@typescript-eslint/parser",
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
+      "import/no-default-export": "error",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          modifiers: ["exported", "const"],
+          format: ["PascalCase", "strictCamelCase"],
+        },
+        {
+          selector: "interface",
+          format: ["PascalCase"],
+        },
+      ],
+    },
+    ignorePatterns: ["node_modules"],
+    overrides: [
+      {
+        files: ["*/app/**/page.tsx", "layout.tsx", "*.config.*"],
+        rules: {
+          "import/no-default-export": "off",
+          "import/prefer-default-export": "error",
+          "@typescript-eslint/naming-convention": "off",
+        },
+      },
+    ],
+  }),
 ];
 
 export default eslintConfig;
