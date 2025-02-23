819a441e309b1b1c8e240a70d3cd422606980235
T-Tkhru <ttkirua1@yahoo.co.jp>
2025-02-23 17:43:36 +0900

    約束内容を修正

--- src/app/_components/PromiseContents.tsx
+++ src/app/_components/PromiseContents.tsx
@@ -1,9 +1,7 @@
-
 import React from "react";
 import { Box, Container, Heading, HStack, Text } from "@yamada-ui/react";
 import { UserSimpleProfile } from "@/lib/type";
 import { UserCard } from "./Card";
-
 
 interface PromiseContentsProps {
   sender: UserSimpleProfile;
@@ -21,12 +19,16 @@ export const PromiseContents: React.FC<PromiseContentsProps> = ({
   return (
     <Container>
       {sender ? (
-        <HStack>
-          <UserCard user={sender} />
-          <Text fontSize="6xl">が</Text>
-          <UserCard user={receiver} />
-          <Text fontSize="6xl">に</Text>
-        </HStack>
+        <Container>
+          <HStack>
+            <UserCard user={sender} />
+            <Text fontSize="6xl">が</Text>
+            <UserCard user={receiver} />
+            <Text fontSize="6xl">に</Text>
+          </HStack>
+          <Text fontSize="3xl">{content}</Text>
+          <Text fontSize="3xl">期限 : {deadline}まで</Text>
+        </Container>
       ) : (
         <Heading size="md" p={4}>
           ようこそ、ゲストさん
