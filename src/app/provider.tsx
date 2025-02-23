f7e1ea89fff62d5f0b0569cc9f5ee01f17e5fc6b
Melonps <kake.kake0311@gmail.com>
2025-02-23 22:56:59 +0900

    [FEAT] liff空のデータをmockする

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,46 +1,15 @@
 "use client";
 
 import { ApolloProvider } from "@apollo/client";
-import { Liff } from "@line/liff";
 import { UIProvider } from "@yamada-ui/react";
-import { useEffect, useState } from "react";
 
 import { theme } from "@/app/theme";
-import { GlobalContext } from "@/contexts/GlobalContext";
 import { client } from "@/lib/apollo-client";
 
 export const LIFFTemplate = ({ children }: { children: React.ReactNode }) => {
-  const [liffObject, setLiffObject] = useState<Liff | null>(null);
-  const [liffError, setLiffError] = useState<string | null>(null);
-
-  // // Execute liff.init() when the app is initialized
-  // useEffect(() => {
-  //   import("@line/liff")
-  //     .then((liff) => liff.default)
-  //     .then((liff) => {
-  //       console.log("LIFF init...");
-  //       liff
-  //         .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
-  //         .then(() => {
-  //           console.log("LIFF init succeeded.");
-  //           setLiffObject(liff);
-  //         })
-  //         .catch((error: Error) => {
-  //           console.log("LIFF init failed.");
-  //           setLiffError(error.toString());
-  //         });
-  //     });
-  // }, []);
-
   return (
     <UIProvider theme={theme}>
-      {/* <GlobalContext.Provider
-        value={{ liff: liffObject, liffError: liffError }}
-      > */}
-      <ApolloProvider client={client}>
-        <div>{children}</div>
-      </ApolloProvider>
-      {/* </GlobalContext.Provider> */}
+      <ApolloProvider client={client}>{children}</ApolloProvider>
     </UIProvider>
   );
 };
