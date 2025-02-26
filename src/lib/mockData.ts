859212b971493df0c9f432b142b5ee679fb42de0
Melonps <kake.kake0311@gmail.com>
2025-02-26 14:18:42 +0900

    [FIX] mockの型がミスってた

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -6,6 +6,7 @@ export const exampleSimpleUser: UserSimpleProfile = {
   image:
     "https://ca.slack-edge.com/T060W6FMWBF-U06D7EVJE4B-5ec28bf084d7-512#.png",
   name: "大塚 遥",
+  id: "testUser1",
 };
 
 export const exampleUser: UserProfile = {
