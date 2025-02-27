9894320c25bbe84b89514f317db90a0b8827bd38
Melonps <kake.kake0311@gmail.com>
2025-02-28 01:25:07 +0900

    [FIX] go backじゃなくてhomeにする

--- src/app/_components/GoBackButton.tsx
+++ src/app/_components/GoBackButton.tsx
@@ -1,41 +1,26 @@
 "use client";
-import { ArrowLeftIcon } from "@yamada-ui/lucide";
+import { HouseIcon } from "@yamada-ui/lucide";
 import { HStack, Icon } from "@yamada-ui/react";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import Link from "next/link";
 
 interface BackButtonProps {
   color?: string;
 }
 
 export function BackButton({ color }: BackButtonProps) {
-  const router = useRouter();
-  const [canGoBack, setCanGoBack] = useState(false);
-
-  useEffect(() => {
-    if (window.history.length > 1) {
-      setCanGoBack(true);
-    }
-  }, []);
-
-  const handleBack = () => {
-    router.back();
-  };
-
-  if (!canGoBack) return null;
-
   return (
     <HStack w="full">
-      <Icon
-        variant="ghost"
-        size="xl"
-        rounded="full"
-        fontSize="xl"
-        p={4}
-        as={ArrowLeftIcon}
-        onClick={handleBack}
-        color={color ? color : "black"}
-      ></Icon>
+      <Link href="/home">
+        <Icon
+          variant="ghost"
+          size="xl"
+          rounded="full"
+          fontSize="xl"
+          p={4}
+          as={HouseIcon}
+          color={color ? color : "black"}
+        ></Icon>
+      </Link>
     </HStack>
   );
 }
