1290939f1470ee20e328ec249364289bea4c12ab
Melonps <kake.kake0311@gmail.com>
2025-03-04 20:56:17 +0900

    [FEAT] タスクの関数を追加

--- reminder/main.py
+++ reminder/main.py
@@ -1,9 +1,10 @@
 import os
+import random
 from dataclasses import dataclass
 from datetime import datetime
 
 from dotenv import load_dotenv
-from line.send_message import send_line_message
+from line.send_message import create_button_template_message, send_line_message
 
 from supabase import Client, create_client
 
@@ -15,6 +16,7 @@ class Promise:
     content: str
     level: str
     due_date: datetime
+    is_reverse: bool
     is_accepted: bool
     completed_at: datetime
     created_at: datetime
@@ -23,25 +25,41 @@ class Promise:
     senderUserId: str
 
 
+@dataclass
+class User:
+    userId: int
+    display_name: str
+
+
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_API_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 current_time = datetime.utcnow().isoformat()
 
-response = (
+response_promises = (
     supabase.table("Promise")
     .select("*")
-    .not_.is_("completedAt", "null")
-    .lte("dueDate", current_time)
+    .is_("completedAt", "null")
+    .not_.is_("receiverUserId", "null")
+    .gt("dueDate", current_time)
     .eq("isAccepted", True)
     .execute()
 )
 
-for row in response.data:
+response_users = supabase.table("User").select("*").execute()
+users = {row["userId"]: row["displayName"] for row in response_users.data}
+
+
+def get_user_name(user_id):
+    return users.get(user_id, "Unknown")
+
+
+for row in response_promises.data:
     promise = Promise(
         id=row["id"],
         content=row["content"],
+        is_reverse=row["direction"],
         level=row["level"],
         due_date=row["dueDate"],
         is_accepted=row["isAccepted"],
@@ -51,8 +69,19 @@ for row in response.data:
         receiverUserId=row["receiverUserId"],
         senderUserId=row["senderUserId"],
     )
-
-    if random.random() < 0.2:
-        send_line_message(
-            user_id=promise.receiverUserId, messages=["約束...忘れてない？"]
-        )
+    print(promise)
+    result = send_line_message(
+        user_id=promise.senderUserId if promise.is_reverse else promise.receiverUserId,
+        messages=[
+            {
+                "type": "text",
+                "text": f"{get_user_name(promise.senderUserId)}と{get_user_name(promise.receiverUserId)}との連絡...忘れてない？",
+            },
+            create_button_template_message(
+                text="約束を確認はこちらこら！",
+                image_url="https://i.gyazo.com/9353b09650abfb3deb5c50227fc5f56a.jpg",
+                liff_url=f"https://example.com/promise/{promise.id}",
+            ),
+        ],
+    )
+    print(result)
